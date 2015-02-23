18873ec728eae85810130587709e20b6a6396bbb
Justina Chen <rcfans@163.com>
2015-02-23 17:53:08 +0800

    fix clerical error

--- CallLogAnalysis.scala
+++ CallLogAnalysis.scala
@@ -2,11 +2,11 @@ import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
-var pathGraph = GraphLoader.edgeListFile(sc, "data/spark_edges10000")
+val pathGraph = GraphLoader.edgeListFile(sc, "data/spark_edges10000")
 
-var ranks = pathGraph.pageRank(0.0001).vertices
+val ranks = pathGraph.pageRank(0.0001).vertices
 
-var services = (sc.textFile("data/spark_vertex"))
+val services = (sc.textFile("data/spark_vertex"))
 	.map(line => line.split(",")).map(fields => (fields(0).toLong, fields(1)))
 
 val ranksByDesc = services.join(ranks).map {
